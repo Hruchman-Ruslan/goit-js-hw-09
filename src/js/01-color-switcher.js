72f62046a7653f5c525aa2c1622bd12b806003b5
Ruslan <Giovingo92@gmail.com>
2023-03-16 09:14:35 +0200

    task one

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,7 +5,6 @@ function getRandomHexColor() {
 const startButton = document.querySelector('button[data-start]');
 const stopButton = document.querySelector('button[data-stop]');
 
-let timerId = null;
 stopButton.disabled = true;
 
 startButton.addEventListener('click', () => {
