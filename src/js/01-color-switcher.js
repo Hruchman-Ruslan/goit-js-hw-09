f18a3f19f0882f7954d6417a0f9960cc80c91f8d
Ruslan <Giovingo92@gmail.com>
2023-03-16 08:30:42 +0200

    first task

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,22 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+
+stopButton.disabled = true;
+
+startButton.addEventListener('click', () => {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+  timerId = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+});
+
+stopButton.addEventListener('click', () => {
+  startButton.disabled = false;
+  stopButton.disabled = true;
+  clearInterval(timerId);
+});
